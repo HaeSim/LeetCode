9c9b83ab86864265b5b84e966772d4652c914b3a
haesim <anjin309@gmail.com>
2022-01-17 19:17:49 +0900

    Time: 170 ms (23.49%), Space: 67.8 MB (11.97%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-     
-        std::ios::sync_with_stdio(false);
-        
-        if(nums.size() == 1)
-            return nums[0];
-        
-        int sum = nums[0];
-        int ans = sum;
-        int temp = ans;
-        for(int i=1; i < nums.size(); i++)
-        {
-            sum += nums[i];
-            ans = max(nums[i], max(sum, ans));
-            
-            if(ans == nums[i]) sum = ans;
-            
-            if(temp == ans && sum < 0)
-                sum = 0;
-            
-            temp = ans;
+    int maxSubArray(vector<int> &nums) {
+        int globalMaxSum = nums[0], currMaxSum = nums[0];
+        for (int ind = 1; ind < nums.size(); ind++) {
+            currMaxSum = max(currMaxSum + nums[ind], nums[ind]);
+            globalMaxSum = max(globalMaxSum, currMaxSum);
         }
-        return ans;
+        return globalMaxSum;
     }
 };
