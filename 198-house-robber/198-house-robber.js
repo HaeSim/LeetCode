6f2e910075300a3f719db2527e2530cf05520019
haesim <anjin309@gmail.com>
2022-01-21 17:38:22 +0900

    Time: 128 ms (7.41%), Space: 38.5 MB (76.22%) - LeetHub

---
+++ 198-house-robber/198-house-robber.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    let dp = new Array(101);
+    if(nums.length < 3) {
+       return Math.max(nums[0], nums[1] === undefined ? 0 : nums[1]);
+    }
+    dp[0] = nums[0]
+    dp[1] = nums[1]
+    dp[2] = dp[0] + nums[2]
+    
+    for(let i=3; i<nums.length; i++) {
+    dp[i] = Math.max(dp[i-2], dp[i-3])+nums[i]
+
+    }
+    return Math.max(dp[nums.length-1], dp[nums.length-2])
+};
